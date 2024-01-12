4c7f6ae7dcb8912f7ff0e17d07890ef128667cc7
andrpavlou <100830163+andrpavlou@users.noreply.github.com>
2024-01-12 14:40:08 +0200

    Add files via upload

---
+++ project0/shopSmart.py
@@ -0,0 +1,58 @@
+# shopSmart.py
+# ------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+#
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+"""
+Here's the intended output of this script, once you fill it in:
+
+Welcome to shop1 fruit shop
+Welcome to shop2 fruit shop
+For orders:  [('apples', 1.0), ('oranges', 3.0)] best shop is shop1
+For orders:  [('apples', 3.0)] best shop is shop2
+"""
+from __future__ import print_function
+import shop
+
+
+def shopSmart(orderList, fruitShops):
+    """
+        orderList: List of (fruit, numPound) tuples
+        fruitShops: List of FruitShops
+    """
+    if len(fruitShops) == 0:
+        return None
+
+    curr_shop = fruitShops[0]
+    min_price = fruitShops[0].getPriceOfOrder((orderList))
+    
+    # iterates all the shops and finds the minimum price of our order list
+    for shops in fruitShops:
+        curr_price = shops.getPriceOfOrder(orderList)
+        if curr_price <= min_price:
+            min_price = curr_price
+            curr_shop = shops
+    # returns the shop that will cost less money to order from
+    return curr_shop
+
+
+if __name__ == '__main__':
+    "This code runs when you invoke the script from the command line"
+    orders = [('apples', 1.0), ('oranges', 3.0)]
+    dir1 = {'apples': 2.0, 'oranges': 1.0}
+    shop1 = shop.FruitShop('shop1', dir1)
+    dir2 = {'apples': 1.0, 'oranges': 5.0}
+    shop2 = shop.FruitShop('shop2', dir2)
+    shops = [shop1, shop2]
+    print("For orders ", orders, ", the best shop is", shopSmart(orders, shops).getName())
+    orders = [('apples', 3.0)]
+    print("For orders: ", orders, ", the best shop is", shopSmart(orders, shops).getName())
